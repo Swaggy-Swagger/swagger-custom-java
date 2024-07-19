7e595ba0e2d510ba30f17f1647b5722ea80c7de5
Jeonghyun Lee <hyeon00@ewhain.net>
2024-07-19 13:36:47 +0900

    feature: order controller methods by line

--- src/main/java/com/test/swaggercustom/config/SwaggerConfig.java
+++ src/main/java/com/test/swaggercustom/config/SwaggerConfig.java
@@ -1,19 +1,54 @@
 package com.test.swaggercustom.config;
 
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Info;
-import org.springdoc.core.models.GroupedOpenApi;
+import com.test.swaggercustom.utils.ControllerMethodOrderUtil;
+import io.swagger.v3.oas.models.Paths;
+import org.springdoc.core.customizers.OpenApiCustomizer;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Controller;
+
+import java.lang.reflect.Method;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
 
 @Configuration
 public class SwaggerConfig {
 
+    private final ApplicationContext applicationContext;
+
+    public SwaggerConfig(ApplicationContext applicationContext) {
+        this.applicationContext = applicationContext;
+    }
+
     @Bean
-    public GroupedOpenApi customOpenApi() {
-        return GroupedOpenApi.builder()
-                .group("custom-api")
-                .pathsToMatch("/api/**")
-                .build();
+    public OpenApiCustomizer sortOperationsCustomizer() {
+        return openApi -> {
+            Map<String, Object> controllers = applicationContext.getBeansWithAnnotation(Controller.class);
+            List<String> sortedPathsOrder = new ArrayList<>();
+            controllers.values().forEach(controller -> {
+                Class<?> controllerClass = controller.getClass();
+                List<Method> orderedMethods = ControllerMethodOrderUtil.getOrderedMethods(controllerClass);
+                orderedMethods.forEach(method -> {
+                    String[] paths = ControllerMethodOrderUtil.getMethodPaths(controllerClass, method);
+                    for (String path : paths) {
+                        if (!sortedPathsOrder.contains(path)) {
+                            sortedPathsOrder.add(path);
+                        }
+                    }
+                });
+            });
+
+            Paths sortedPaths = new Paths();
+            sortedPathsOrder.forEach(path -> {
+                if (openApi.getPaths().get(path) != null) {
+                    sortedPaths.addPathItem(path, openApi.getPaths().get(path));
+                }
+            });
+            openApi.setPaths(sortedPaths);
+        };
     }
 }
+
